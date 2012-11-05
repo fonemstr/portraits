e286e727034faf4167934657e52be6bc15f96a40
David Demers <fonemstr@gmail.com>
2012-11-05 08:25:54 -0500

    add spree preferences

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,19 +29,19 @@ role :db,  "portraitsinfabric.com", :primary => true # This is where Rails migra
 #role :db,  "your slave db-server here"
 
 # if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
+after "deploy:restart", "deploy:cleanup"
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
 # If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
 
 namespace :deploy do
   namespace :assets do
---
+++ config/initializers/spree_config.rb
@@ -0,0 +1,7 @@
+Spree.config do |config|
+  # Example:
+  # Uncomment to override the default site name.
+  config.site_name = "Portraits in Fabric"
+  config.allow_ssl_in_production = 'false'
+  config.auto_capture = 'true'
+end
