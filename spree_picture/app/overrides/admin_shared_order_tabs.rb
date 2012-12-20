d6cdaa209a19c8770acd0adf60c92417393bed0e
David Demers <fonemstr@gmail.com>
2012-12-20 13:32:15 -0500

    fix order admin tabs for order pictures

--- spree_picture/app/overrides/admin_shared_order_tabs.rb
+++ spree_picture/app/overrides/admin_shared_order_tabs.rb
@@ -1,7 +1,7 @@
 Deface::Override.new(:virtual_path => %q{spree/admin/shared/_order_tabs},
                      :name => %q{add picture to order tabs},
                      :insert_bottom => %q{[data-hook="admin_order_tabs"]},
-                     :text => %q{<li<%= 'class="active"' if current == 'Pictures' %>>          
+                     :text => %q{<li<%== 'class="active"' if current == 'Pictures' %>>          
           <%= link_to_with_icon 'icon-picture', "Pictures", admin_order_pictures_url(@order) %>
         </li>},
                      :disabled => false)
